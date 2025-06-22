689219d1dd98826abd1e8c26c65aa760500f19e4
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-22 17:13:30 +0500

    Accordion Project Completed
    list rendering
    conditional rendering
    useState

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import accordionsData from "./data";
+import Question from "./Question";
+
+const App = () => {
+  return (
+    <section className='bg-purple-900 min-h-screen flex justify-center items-center'>
+      <div className='bg-white shadow-lg max-w-3xl w-full p-6 rounded space-y-6'>
+        <h3 className='text-2xl font-bold text-gray-800'>
+          Question and Answers <br /> About Login
+        </h3>
+
+        <div className='space-y-4 '>
+          {accordionsData.map((accordion) => (
+            <Question key={accordion.id} {...accordion} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/Question.jsx
@@ -0,0 +1,24 @@
+import { FaPlusCircle } from "react-icons/fa";
+import { useState } from "react";
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  let description = showInfo ? info : "";
+  return (
+    <>
+      <div>
+        <div className='flex justify-between items-center bg-purple-100 px-4 py-2 gap-2 rounded-md border border-gray-400 shodow-lg'>
+          <p>{title}</p>
+          <button onClick={() => setShowInfo((prevState) => !prevState)}>
+            <FaPlusCircle className='text-purple-600 text-xl hover:scale-110 transition' />
+          </button>
+        </div>
+        <p className='mt-2 leading-relaxed text-sm text-gray-700'>
+          {showInfo ? info : description}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Question;
