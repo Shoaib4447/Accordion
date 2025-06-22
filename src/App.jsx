0e6ebab5474795afbc365795ebc7dbc48d84c9db
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-22 17:14:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Question from "./Question";
 
 const App = () => {
   return (
-    <section className='bg-purple-900 min-h-screen flex justify-center items-center'>
+    <section className='bg-purple-700 min-h-screen flex justify-center items-center'>
       <div className='bg-white shadow-lg max-w-3xl w-full p-6 rounded space-y-6'>
         <h3 className='text-2xl font-bold text-gray-800'>
           Question and Answers <br /> About Login
